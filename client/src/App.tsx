954ab7bb94681772588cbfba6249a0d9fab36ef7
Beca <beca@MacBook-Air.Home>
2024-10-24 13:53:28 +0100

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<h1> Dashboard</h1>} />
+          <Route path="/auth" element={<h1> SignIn</h1>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;

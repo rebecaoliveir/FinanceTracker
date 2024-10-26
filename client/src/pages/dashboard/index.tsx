b1bb4ba187113051ba4cc3f1aa3cc01aaea4e80a
Beca <beca@Becas-Air.Home>
2024-10-26 16:15:23 +0100

    Connected to Server

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -1,5 +1,28 @@
+import { useUser } from "@clerk/clerk-react";
+import { FinancialRecordForm } from "./financial-record-form.tsx";
+import { FinancialRecordList } from "./financial-record-list.tsx";
+import "./financial-record.css";
+import { useFinancialRecords } from "../../contexts/financial-record-context.tsx";
+import { useMemo } from "react";
 export const Dashboard = () => {
+  const { user } = useUser();
+  const { records } = useFinancialRecords();
 
-    return <h1> Dashboard</h1>
-  
-  };
+  const totalMonthly = useMemo(() => {
+    let totalAmount = 0;
+    records.forEach((record) => {
+      totalAmount += record.amount;
+    });
+
+    return totalAmount;
+  }, [records]);
+
+  return (
+    <div className="dashboard-container">
+      <h1> Welcome {user?.firstName}! Here Are Your Finances:</h1>
+      <FinancialRecordForm />
+      <div>Total Monthly: ${totalMonthly}</div>
+      <FinancialRecordList />
+    </div>
+  );
+};
